a7dfc9f95a1746c8a135aa1788b77efa7e9bf0cd
Yuri Soares <yurisoaresm@outlook.com>
2023-01-01 11:58:59 -0300

    feat: create countdown component

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,10 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="pt-br">
       <Head>
+        <link rel="shortcut icon" href="favicon.png" type="image/png" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Rajdhani:wght@600&display=swap" rel="stylesheet" />
