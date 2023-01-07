025118154690f948015745ed6d394101e8865ec5
Yuri Soares <yurisoaresm@outlook.com>
2023-01-06 21:48:23 -0300

    feat: add functionality of storage data with cookies

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
 import type { AppProps } from 'next/app';
 
-import { ChallengesProvider } from '../contexts/ChallengesContext';
-
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChallengesProvider>
-        <Component {...pageProps} />
-    </ChallengesProvider>
+    <Component {...pageProps} />
   )
 }
