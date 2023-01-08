250a06702f006736d9fbe154e746c894cda2498a
Yuri Soares <yurisoaresm@outlook.com>
2023-01-07 21:32:42 -0300

    refactor: change time to 30 minutes

--- src/contexts/CountdownContext.tsx
+++ src/contexts/CountdownContext.tsx
@@ -22,7 +22,7 @@ let countdownTimeout: NodeJS.Timeout;
 export function CountdownProvider({ children }: CountdownProviderProps) {
   const { startNewChallenge } = useContext(ChallengesContext);
 
-  const [time, setTime] = useState(0.05 * 60); // cada minuto tem 60 segundos
+  const [time, setTime] = useState(30 * 60); // cada minuto tem 60 segundos
   const [isActive, setIsActive] = useState(false);
   const [hasFinished, setHasFinished] = useState(false);
 
